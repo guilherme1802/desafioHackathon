ee8e27d2f7261e724db9822790e0c494a4189ad9
Guilherme Rocha <gui1802@hotmail.com>
2019-10-19 01:52:48 -0300

    Projeto criado

---
+++ src/main/java/br/com/herois/api/DesafioHackathonApplication.java
@@ -0,0 +1,13 @@
+package br.com.herois.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/herois/api/DesafioHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.herois.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
